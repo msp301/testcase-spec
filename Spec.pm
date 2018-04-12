c4371795d80462446937689aee2180c6517fac0a
Martin Pritchard <martin@martinpritchard.com>
2018-04-12 12:34:51 +0100

    Add POD

--- Spec.pm
+++ Spec.pm
@@ -1,5 +1,38 @@
 package TestCase::Spec;
 
+=head1 NAME
+
+TestCase::Spec
+
+=head1 DESCRIPTION
+
+This module is intended to provide some syntactic sugar for writing unit tests
+using RSpec style notation.
+
+The API is intended to closely follow the Perl BDD framework Test::Spec whilst
+providing compatibility to use TestCase::Spec with other test frameworks
+e.g Test::Class.
+
+=head1 EXAMPLE USAGE
+
+    use TestCase::Spec;
+
+    describe 'A kitten' => sub {
+        my $kitten = Kitten->new();
+
+        describe 'when stroked' => sub {
+            it 'meows' => sub {
+                is( $kitten->stroke(), 'meow', TestCase::Spec::test_name );
+            };
+        };
+    };
+
+    # Generates the output:
+    # ok 1 - A kitten when stroked meows
+    # 1..1
+
+=cut
+
 use Exporter qw( import );
 use Test::More;
 
